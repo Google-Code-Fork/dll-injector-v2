a4c0262495e9bb1aa51d03427cdad6b324006bdc
Gugap130@gmail.com <Gugap130@gmail.com@1a635b12-450f-0d4e-8f23-9a0e66637e95>
2014-01-04 12:24:50 +0000

    work on PEFile

--- trunk/DLL Injector/PEFile.h
+++ trunk/DLL Injector/PEFile.h
@@ -1,6 +1,8 @@
-#include <Windows.h>
 #ifndef PE_FILE_H
 #define PE_FILE_H
+
+#include <Windows.h>
+#include <string>
 
 class PEFile
 {
@@ -10,18 +12,22 @@ public:
 
 	void SaveFile(void) const;
 
+	void ExpandLastSection(size_t size);
+
 	size_t GetFileSize(void) const;
 	size_t GetNumberOfSections(void) const;
 
-	void AddSection(char const* name, size_t size);
-	void RemoveSection(int index);
-	void RemoveSection(char const* name);
-private:
-	void LoadFile(void);
 
-	char const*				m_filePath;
-	char*					m_fileData;
+private:
+	static size_t AlignSize(size_t size, size_t alingment);
+	void LoadFile(void);
+	void ReallocateBuffer(size_t size);
+	void UpdateHeaderPointers(void);
+
+	std::string				m_filePath;
+	char*					m_fileBuffer;
 	size_t					m_fileSize;
+	size_t					m_bufferSize;
 
 	IMAGE_DOS_HEADER*		m_dosHeader;
 	IMAGE_NT_HEADERS*		m_ntHeaders;
